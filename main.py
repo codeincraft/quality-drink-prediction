12b3dae9b63c6f88a1ec2308e5378d850d0722f5
Codeincraft <codeincraft@gmail.com>
2026-07-22 20:14:45 +0100

    add wine quality dataset

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from qualityDrinks import logger
 from qualityDrinks.pipeline.stage_01_data_injestion import DataInjestionTrainingPipeline
+from qualityDrinks.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -10,3 +11,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_injestion = DataValidationTrainingPipeline()
+    data_injestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed!<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
--- src/qualityDrinks/config/configuration.py
+++ src/qualityDrinks/config/configuration.py
@@ -1,6 +1,6 @@
 from qualityDrinks.constants import *
 from qualityDrinks.utils.common import read_yaml, create_directories
-from qualityDrinks.entity.config_entity import DataIngestionConfig
+from qualityDrinks.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -24,3 +24,19 @@ class ConfigurationManager:
             unzip_dir=config.unzip_dir
         )
         return data_ingestion_config
+    
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
