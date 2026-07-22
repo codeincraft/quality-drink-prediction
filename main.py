64a302c7312e52e09cea5ddf15ba1a2cd8685dfc
Codeincraft <codeincraft@gmail.com>
2026-07-22 22:54:41 +0100

    Data transformation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from qualityDrinks import logger
 from qualityDrinks.pipeline.stage_01_data_injestion import DataInjestionTrainingPipeline
 from qualityDrinks.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from qualityDrinks.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -17,6 +18,18 @@ STAGE_NAME = "Data Validation Stage"
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
     data_injestion = DataValidationTrainingPipeline()
+    data_injestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed!<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+    
+    
+    
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_injestion = DataTransformationTrainingPipeline()
     data_injestion.main()
     logger.info(f">>>>> stage {STAGE_NAME} completed!<<<<<\n\nx==========x")
 except Exception as e:
--- src/qualityDrinks/config/configuration.py
+++ src/qualityDrinks/config/configuration.py
@@ -1,6 +1,6 @@
 from qualityDrinks.constants import *
 from qualityDrinks.utils.common import read_yaml, create_directories
-from qualityDrinks.entity.config_entity import (DataIngestionConfig, DataValidationConfig)
+from qualityDrinks.entity.config_entity import (DataIngestionConfig, DataTransformationConfig, DataValidationConfig)
 
 class ConfigurationManager:
     def __init__(
@@ -40,3 +40,16 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path,
+        )
+
+        return data_transformation_config
