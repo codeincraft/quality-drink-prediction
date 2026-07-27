a408d481ddec8f58d8f771f138b11fbeed7249b2
Codeincraft <codeincraft@gmail.com>
2026-07-27 23:47:23 +0100

    model training

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from qualityDrinks import logger
 from qualityDrinks.pipeline.stage_01_data_injestion import DataInjestionTrainingPipeline
 from qualityDrinks.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 from qualityDrinks.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from qualityDrinks.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -35,3 +36,13 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+    
+STAGE_NAME = " Model Trainer stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    data_injestion = ModelTrainerTrainingPipeline()
+    data_injestion.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
